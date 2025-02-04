806f8c2e191f05ddb2dd181277adbde25255637b
neuro <yamsh@yandex.ru>
2025-02-04 20:10:23 +0500

    constraints to comply with certain pattern added

--- password_generator.py
+++ password_generator.py
@@ -1,21 +1,28 @@
+import re
 import string
 import secrets
 
 # Define function "generate_password"
-def generate_password(length):
+def generate_password(length, nums, special_chars, uppercase, lowercase):
     letters = string.ascii_letters
     digits = string.digits
     symbols = string.punctuation
     # combine all the variables into one list
     all_characters = letters + digits + symbols
     print("All characters:", all_characters)
-    password = ''
-    # Generate password
-    for _ in range(length):
-        password += secrets.choice(all_characters)
-    return password
-new_password = generate_password(8)
-
-    
+    while True:
+            password = ''
+            # Generate password
+            for _ in range(length):
+                password += secrets.choice(all_characters)
+            constraints = [
+                 (nums, '')
+            ]
+            return password
+        # new_password = generate_password(8)
+        # print(new_password)
+pattern = re.compile('i')
+quote = 'Not all those who wander lost'
+print(pattern.search(quote))
     
     
