b00423971b66200d4814a7f9e5ac677af26b6b0e
neuro <yamsh@yandex.ru>
2025-02-17 15:43:06 +0500

    creates list of unvisited nodes and dictionary of paths with list/dictionary comprehension

--- shortest_path_algorithm.py
+++ shortest_path_algorithm.py
@@ -1,13 +1,10 @@
 my_graph = {'A':[('B', 3),('D', 1)], 'B':[('A', 3),('C', 4)], 'C':[('B', 4),('D', 7)], 'D':[('A', 1),('C', 7)]}
 
 def shortest_path(graph, start):
-    unvisited = []
+    # Create a list of unvisited nodes using list constructor
+    unvisited = list(graph)
     distances = {}
-    for node in graph:
-        unvisited.append(node)
-        if node == start:
-            distances[node] = 0
-        else:
-            distances[node] = float('inf')
+    # Create a dictionary of paths using dictionary comprehension
+    paths = {key: [] for key in graph}
     print(f'Unvisited: {unvisited}\nDistances: {distances}')
 shortest_path(my_graph, 'A')
