a48fc01fb2e027f46370631085105378fba785b8
neuro <yamsh@yandex.ru>
2025-02-19 23:24:29 +0500

    add the starting point to the paths dictionary

--- shortest_path_algorithm.py
+++ shortest_path_algorithm.py
@@ -3,9 +3,11 @@ my_graph = {'A':[('B', 3),('D', 1)], 'B':[('A', 3),('C', 4)], 'C':[('B', 4),('D'
 def shortest_path(graph, start):
     # Create a list of unvisited nodes using list constructor
     unvisited = list(graph)
-    
+    # create a distionary of distances using dictionary comprehension
     distances = {node: 0 if node == start else float('inf') for node in graph}
     # Create a dictionary of paths using dictionary comprehension
     paths = {key: [] for key in graph}
+    # Add the starting point to the paths dictionary
+    paths[start].append(start)
     print(f'Unvisited: {unvisited}\nDistances: {distances}')
 shortest_path(my_graph, 'A')
